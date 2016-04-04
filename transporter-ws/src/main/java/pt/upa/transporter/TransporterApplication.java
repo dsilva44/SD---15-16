25a85f1c295c3fdc96cbff559cf6937f9688ce09
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-04 20:55:00 +0100

    Add ping code that pass ping test in transporter-ws
    Add logic to create stub in TransporterApplication
    Closes #13

--- transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
+++ transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
@@ -1,10 +1,68 @@
 package pt.upa.transporter;
 
-public class TransporterApplication {
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.transporter.ws.TransporterPort;
 
-	public static void main(String[] args) throws Exception {
-		System.out.println(TransporterApplication.class.getSimpleName() + " starting...");
+import javax.xml.ws.Endpoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TransporterApplication {
+	static private final Logger log = LogManager.getRootLogger();
+
+	public static void main(String[] args) {
+		// Check arguments
+		if (args.length < 3) {
+			log.error("Argument(s) missing!");
+			log.error("Usage: java "+ TransporterApplication.class.getName() +" + uddiURL wsName wsURL");
+			return;
+		}
+
+		String uddiURL = args[0];
+		String name = args[1];
+		String url = args[2];
+
+		Endpoint endpoint = null;
+		UDDINaming uddiNaming = null;
+		try {
+			endpoint = Endpoint.create(new TransporterPort());
+
+			// publish endpoint
+			log.info("Starting: " + url);
+			endpoint.publish(url);
+
+			// publish to UDDI
+			log.info("Publishing '"+ name + "' to UDDI at "+ uddiURL);
+			uddiNaming = new UDDINaming(uddiURL);
+			uddiNaming.rebind(name, url);
+
+			// wait
+			log.info("Awaiting connections");
+			System.out.println("Press enter to shutdown");
+			System.in.read();
+
+		} catch (Exception e) {
+			log.error("Caught exception", e);
+		} finally {
+			try {
+				if (endpoint != null) {
+					// stop endpoint
+					endpoint.stop();
+					log.info("Stopped " + url);
+				}
+			} catch (Exception e) {
+				log.error("Caught exception when stopping", e);
+			}
+			try {
+				if (uddiNaming != null) {
+					// delete from UDDI
+					uddiNaming.unbind(name);
+					log.info("Deleted '"+ name +"' from UDDI");
+				}
+			} catch (Exception e) {
+				log.error("Caught exception when deleting", e);
+			}
+		}
 
 	}
-
 }
--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,7 +1,49 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import javax.jws.WebService;
+import java.util.List;
 
-	// TODO
+@WebService(
+        endpointInterface = "pt.upa.transporter.ws.TransporterPortType",
+        wsdlLocation = "transporter.1_0.wsdl",
+        portName = "TransporterPort",
+        targetNamespace = "http://ws.transporter.upa.pt/",
+        serviceName = "TransporterService"
+)
+public class TransporterPort implements TransporterPortType {
 
+    @Override
+    public String ping(String name) {
+        if (name == null) {return "Pong!";}
+        else return "Pong " + name + "!";
+    }
+
+    @Override
+    public JobView requestJob(String origin, String destination, int price) throws BadLocationFault_Exception, BadPriceFault_Exception {
+        //TODO requestJob
+        return null;
+    }
+
+    @Override
+    public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+        //TODO decideJob
+        return null;
+    }
+
+    @Override
+    public JobView jobStatus(String id) {
+        //TODO jobStatus
+        return null;
+    }
+
+    @Override
+    public List<JobView> listJobs() {
+        //TODO listJobs
+        return null;
+    }
+
+    @Override
+    public void clearJobs() {
+        //TODO clearJobs
+    }
 }
