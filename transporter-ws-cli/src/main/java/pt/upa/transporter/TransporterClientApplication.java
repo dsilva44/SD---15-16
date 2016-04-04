2befe0abf8c6c3af0598a409ecf98adefda0f275
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-04 21:26:03 +0100

    Add Transporter Client Code

--- transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
@@ -1,9 +1,24 @@
 package pt.upa.transporter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pt.upa.transporter.ws.TransporterPortType;
+import pt.upa.transporter.ws.cli.TransporterClient;
+
 public class TransporterClientApplication {
+	static private final Logger log = LogManager.getRootLogger();
 
 	public static void main(String[] args) throws Exception {
-		System.out.println(TransporterClientApplication.class.getSimpleName() + " starting...");
 
+		if (args.length < 2) {
+			log.error("Argument(s) missing!");
+			log.error("Usage: java "+ TransporterClientApplication.class.getName() +" + uddiURL wsName");
+			return;
+		}
+
+		TransporterClient client = new TransporterClient(args[0], args[1]);
+		TransporterPortType port = client.getPort();
+
+		String result = port.ping("friend");
 	}
 }
--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -1,7 +1,59 @@
 package pt.upa.transporter.ws.cli;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.transporter.ws.TransporterPortType;
+import pt.upa.transporter.ws.TransporterService;
+
+import javax.xml.ws.BindingProvider;
+import java.util.Map;
+
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
+
 public class TransporterClient {
+	static private final Logger log = LogManager.getRootLogger();
 
-	// TODO
+	private String uddiURL;
+	private String wsName;
+	private TransporterPortType port;
 
+	public TransporterClient(String uddiURL, String wsName) throws Exception {
+		this.uddiURL = uddiURL;
+		this.wsName = wsName;
+
+		log.info("Contacting UDDI at " + uddiURL);
+		UDDINaming uddiNaming = new UDDINaming(uddiURL);
+
+		log.info("Looking for '" + wsName + "'");
+		String endpointAddress = uddiNaming.lookup(wsName);
+
+		if (endpointAddress == null) {
+			log.info("Not found!");
+			return;
+		} else {
+			log.info("Found " + endpointAddress);
+		}
+
+		log.info("Creating stub ...");
+		TransporterService service = new TransporterService();
+		port = service.getTransporterPort();
+
+		log.info("Setting endpoint address ...");
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+	}
+
+	public TransporterPortType getPort() {
+		return port;
+	}
+
+	public String getUddiURL() {
+		return uddiURL;
+	}
+
+	public String getWsName() {
+		return wsName;
+	}
 }
