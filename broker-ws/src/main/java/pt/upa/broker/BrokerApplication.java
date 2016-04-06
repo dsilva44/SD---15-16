b17d38cbc9ce6c16ffee6d4a6fe7f2f5e1b30aa5
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-06 18:51:44 +0100

    Add example of Broker and Transport interaction
    Add transporter-ws-cl do broker pom dependency

--- broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
+++ broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
@@ -1,10 +1,19 @@
 package pt.upa.broker;
 
+import pt.upa.transporter.ws.cli.TransporterClient;
+
 public class BrokerApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BrokerApplication.class.getSimpleName() + " starting...");
 
+		// Example of the project organization
+		// UML - http://goo.gl/nkmCh1
+		String uddiURL = "http://localhost:9090";
+		String wsName = "UpaTransporter1";
+		TransporterClient client = new TransporterClient(uddiURL, wsName);
+
+		System.out.println(client.ping("friend"));
 	}
 
 }
