3d787d1ca59cfa9c81fbcb53270da7140644ad84
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-09 19:26:02 +0100

    Add bootStrap for transport-ws
    Initialize Transporter parity ans work cities

--- transporter-ws/src/main/java/pt/upa/transporter/ws/DomainManager.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/DomainManager.java
@@ -1,5 +1,44 @@
 package pt.upa.transporter.ws;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DomainManager {
-    //TODO
+    private final String parity;
+    private List<String> workCities;
+    private List<JobView> jobs;
+
+    public DomainManager(String transporterName) {
+        int tNum = Integer.parseInt(transporterName.substring(transporterName.length() - 1));
+        if (tNum % 2 == 0) { parity = "EVEN"; }
+        else parity = "ODD";
+    }
+
+    private void init() {
+        switch (parity) {
+            case "EVEN":
+                workCities = new ArrayList<>(Arrays.asList(
+                        "Lisboa", "Leiria", "Santarém", "Castelo Branco", "Coimbra", "Aveiro", "Viseu", "Guarda",
+                        "Porto", "Braga", "Viana do Castelo", "Vila Real", "Bragança"));
+            case "ODD":
+                workCities = new ArrayList<>(Arrays.asList(
+                        "Lisboa", "Leiria", "Santarém", "Castelo Branco", "Coimbra", "Aveiro", "Viseu", "Guarda",
+                        "Setúbal", "Évora", "Portalegre", "Beja", "Faro"));
+        }
+    }
+
+    public String getParity() {
+        return parity;
+    }
+
+    public boolean decideResponde() {
+        //TODO RequestJob
+        return false;
+    }
+
+    public void TransportSimulation() {
+        // TODO
+    }
 }
