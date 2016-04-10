d5f8584928274b46c72b2b1c8d5fd020cc7d9ca1
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-10 17:02:17 +0100

    Closes #20

---
+++ transporter-ws/src/main/java/pt/upa/transporter/exception/TransporterEndpointExeption.java
@@ -0,0 +1,9 @@
+package pt.upa.transporter.exception;
+
+public class TransporterEndpointExeption extends TransporterException {
+    private static final long serialVersionUID = 1L;
+
+    public TransporterEndpointExeption(String msg) {
+        super(msg);
+    }
+}
