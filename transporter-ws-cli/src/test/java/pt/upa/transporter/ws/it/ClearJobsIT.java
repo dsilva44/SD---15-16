7e856becd1a65a7e201b833842e3610b2c79e82f
Miguel Carvalho <migueloak59@hotmail.com>
2016-04-12 20:53:33 +0100

    ClearJobsIT now populates TransporterDomain

--- transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/ClearJobsIT.java
+++ transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/ClearJobsIT.java
@@ -4,10 +4,22 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class ClearJobsIT extends AbstractIntegrationTest {
+import pt.upa.transporter.ws.BadLocationFault_Exception;
+import pt.upa.transporter.ws.BadPriceFault_Exception;
 
+public class ClearJobsIT extends AbstractIntegrationTest {
+	
+	private String validLocation1 = "Lisboa";
+    private String validLocation2 = "Leiria";
+    private int validPrice1 = 20;
+    private int validPrice2 = 50;
+    
     @Test
-    public void successClearJobsShouldEmptyList() {
+    public void successClearJobsShouldEmptyList() throws BadLocationFault_Exception, BadPriceFault_Exception {
+    	
+    	client1.requestJob(validLocation1, validLocation2, validPrice1);
+    	client1.requestJob(validLocation2, validLocation1, validPrice2);
+    	
     	client1.clearJobs();
         assertTrue(client1.listJobs().isEmpty());
     }
