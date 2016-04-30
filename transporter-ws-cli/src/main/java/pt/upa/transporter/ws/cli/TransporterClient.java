41fb2c773ffb0eeecec33af36ef625138d206300
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-04-30 08:55:45 +0100

    Refactor Transporter Client

--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -6,7 +6,6 @@ import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 import pt.upa.transporter.exception.TransporterClientException;
 import pt.upa.transporter.ws.*;
 
-import javax.xml.registry.JAXRException;
 import javax.xml.ws.BindingProvider;
 import java.util.List;
 import java.util.Map;
@@ -16,46 +15,45 @@ import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 public class TransporterClient implements TransporterPortType {
 	static private final Logger log = LogManager.getRootLogger();
 
+	private String wsName;
+	private String uddiURL;
 	private TransporterPortType port;
 	private String wsURL;
 
 	public TransporterClient(String uddiURL, String wsName) {
+		this.uddiURL = uddiURL;
+		this.wsName = wsName;
+		uddiLookup();
+		createStub();
+	}
 
-		String endpointAddress;
+	public TransporterClient(String wsURL) {
+		this.wsURL = wsURL;
+		createStub();
+	}
+
+	/** UDDI lookup */
+	private void uddiLookup() throws TransporterClientException {
 		try {
 			log.info("Contacting UDDI at " + uddiURL);
 			UDDINaming uddiNaming = new UDDINaming(uddiURL);
 
 			log.info("Looking for '" + wsName + "'");
-			endpointAddress = uddiNaming.lookup(wsName);
+			wsURL = uddiNaming.lookup(wsName);
 
-		} catch (JAXRException e) {
+		} catch (Exception e) {
 			String msg = String.format("Client failed lookup on UDDI at %s!", uddiURL);
-			throw  new TransporterClientException(msg, e);
-
+			throw new TransporterClientException(msg, e);
 		}
 
-		if (endpointAddress == null) {
-			log.info("Not found!");
+		if (wsURL == null) {
 			String msg = String.format("Service with name %s not found on UDDI at %s", wsName, uddiURL);
-			throw  new TransporterClientException(msg);
-		} else {
-			log.info("Found " + endpointAddress);
-			this.wsURL = endpointAddress;
+			throw new TransporterClientException(msg);
 		}
-
-		log.info("Creating stub ...");
-		TransporterService service = new TransporterService();
-		port = service.getTransporterPort();
-
-		log.info("Setting endpoint address ...");
-		BindingProvider bindingProvider = (BindingProvider) port;
-		Map<String, Object> requestContext = bindingProvider.getRequestContext();
-		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
 	}
 
-	public TransporterClient(String wsURL) {
-		this.wsURL = wsURL;
+	/** Stub creation and configuration */
+	private void createStub() {
 		log.info("Creating stub ...");
 		TransporterService service = new TransporterService();
 		port = service.getTransporterPort();
@@ -66,7 +64,7 @@ public class TransporterClient implements TransporterPortType {
 		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, wsURL);
 	}
 
-	public String getWsURL() { return wsURL; }
+	/*-----------------------------------------------remote invocation methods----------------------------------------*/
 
 	@Override
 	public String ping(String name) {
