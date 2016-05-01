c2d62aef293272047b8b583a5c55e36890e6b34a
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-05-01 10:54:51 +0100

    Add global variables

--- UpaCertificateDeploy.sh
+++ UpaCertificateDeploy.sh
@@ -6,87 +6,125 @@
 
 VERSION=0.1.0
 
+
+# --- Variables ----------------------------------------------------
+# alias
+ca_alias=UpaCA
+broker_alias=UpaBroker
+t1_alias=UpaTransporter1
+t2_alias=UpaTransporter2
+
+# keystore
+ca_jks=UpaCA.jks
+broker_jks=UpaBroker.jks
+t1_jks=UpaTransporter1.jks
+t2_jks=UpaTransporter2.jks
+
+# keystore password
+ca_pass=passUpaCA
+broker_pass=passUpaBroker
+t1_pass=passUpaTransporter1
+t2_pass=passUpaTransporter2
+
+# certificates request
+broker_csr=UpaBroker.csr
+t1_csr=UpaTransporter1.csr
+t2_csr=UpaTransporter2.csr
+
+# certificates
+ca_cer=UpaCA.cer
+broker_cer=UpaBroker.cer
+t1_cer=UpaTransporter1.cer
+t2_cer=UpaTransporter2.cer
+
 # --- UpaCA (Trust Root)--------------------------------------------
 # create key pair and new keystore
 keytool -genkeypair \
-        -alias "UpaCA" \
+        -alias ${ca_alias} \
         -keyalg RSA -keysize 2048 \
-        -keypass "passUpaCA" -validity 90 -storepass "passUpaCA" \
-        -keystore UpaCA.jks -ext bc=ca:true \
+        -keypass ${ca_pass} -validity 90 -storepass ${ca_pass} \
+        -keystore ${ca_jks} -ext bc=ca:true \
         -dname "CN=UpaCA, OU=T_27, O=IST, L=Lisbon, S=Lisbon, C=PT"
 # create self sign certificate
 keytool -export \
-        -keystore UpaCA.jks \
-        -alias "UpaCA" \
-        -storepass "passUpaCA" \
+        -keystore ${ca_jks} \
+        -alias ${ca_alias} \
+        -storepass ${ca_pass} \
         -file UpaCA.cer
 
 # --- UpaBroker ------------------------------------------------------
 # create key pair and new keystore
 keytool -genkeypair \
-        -alias "UpaBroker" \
+        -alias ${broker_alias} \
         -keyalg RSA -keysize 2048 \
-        -keypass "passUpaBroker" -validity 90 -storepass "passUpaBroker" \
-        -keystore UpaBroker.jks \
+        -keypass ${broker_pass} -validity 90 -storepass ${broker_pass} \
+        -keystore ${broker_jks} \
         -dname "CN=UpaBroker, OU=T_27, O=IST, L=Lisbon, S=Lisbon, C=PT"
 # create certificate request
 keytool -certreq \
-        -keystore UpaBroker.jks \
-        -storepass "passUpaBroker" \
-        -alias "UpaBroker" \
-        -file UpaBroker.csr
+        -keystore ${broker_jks} \
+        -storepass ${broker_pass} \
+        -alias ${broker_alias} \
+        -file ${broker_csr}
 
 # --- UpaTransporter1 ------------------------------------------------
 # create key pair and new keystore
 keytool -genkeypair \
-        -alias "UpaTransporter1" \
+        -alias ${t1_alias} \
         -keyalg RSA -keysize 2048 \
-        -keypass "passUpaTransporter1" -validity 90 -storepass "passUpaTransporter1" \
-        -keystore UpaTransporter1.jks \
+        -keypass ${t1_pass} -validity 90 -storepass ${t1_pass} \
+        -keystore ${t1_jks} \
         -dname "CN=UpaTransporter1, OU=T_27, O=IST, L=Lisbon, S=Lisbon, C=PT"
 # create certificate request
 keytool -certreq \
-        -keystore UpaTransporter1.jks \
-        -storepass passUpaTransporter1 \
-        -alias UpaTransporter1 \
-        -file UpaTransporter1.csr
+        -keystore ${t1_jks} \
+        -storepass ${t1_pass} \
+        -alias ${t1_alias} \
+        -file ${t1_csr}
 
 # --- UpaTransporter2 ------------------------------------------------
 # create key pair and new keystore
 keytool -genkeypair \
-        -alias UpaTransporter2 \
+        -alias ${t2_alias} \
         -keyalg RSA -keysize 2048 \
-        -keypass passUpaTransporter2 -validity 90 -storepass passUpaTransporter2 \
-        -keystore UpaTransporter2.jks \
+        -keypass ${t2_pass} -validity 90 -storepass ${t2_pass} \
+        -keystore ${t2_jks} \
         -dname "CN=UpaTransporter2, OU=T_27, O=IST, L=Lisbon, S=Lisbon, C=PT"
 # create certificate request
 keytool -certreq \
-        -keystore UpaTransporter2.jks \
-        -storepass passUpaTransporter2 \
-        -alias UpaTransporter2 \
-        -file UpaTransporter2.csr
+        -keystore ${t2_jks} \
+        -storepass ${t2_pass} \
+        -alias ${t2_alias} \
+        -file ${t2_csr}
 
 # --- Sign certificates ----------------------------------------------
 # create UpaBroker certificate sign by UpaCA
 keytool -gencert \
-        -keystore UpaCA.jks \
-        -storepass passUpaCA \
-        -alias UpaCA \
-        -infile UpaBroker.csr \
-        -outfile UpaBroker.cer
+        -keystore ${ca_jks} \
+        -storepass ${ca_pass} \
+        -alias ${ca_alias} \
+        -infile ${broker_csr} \
+        -outfile ${broker_cer}
 # create UpaTransporter1 certificate sign by UpaCA
 keytool -gencert \
-        -keystore UpaCA.jks \
-        -storepass passUpaCA \
-        -alias UpaCA \
-        -infile UpaTransporter1.csr \
-        -outfile UpaTransporter1.cer
+        -keystore ${ca_jks} \
+        -storepass ${ca_pass} \
+        -alias ${ca_alias} \
+        -infile ${t1_csr} \
+        -outfile $t1_cer
 # create UpaTransporter2 certificate sign by UpaCA
 keytool -gencert \
-        -keystore UpaCA.jks \
-        -storepass passUpaCA \
-        -alias UpaCA \
-        -infile UpaTransporter2.csr \
-        -outfile UpaTransporter2.cer
+        -keystore ${ca_jks} \
+        -storepass ${ca_pass} \
+        -alias ${ca_alias} \
+        -infile ${t2_csr} \
+        -outfile ${t2_cer}
 
 # --- Deploy Certificates --------------------------------------------
+#caFolder = ./ca-ws/src/main/resources/
+#brokerFolder = ./broker-ws/src/main/resources/
+#transporterFolder = ./transporter-ws/src/main/resources/
+
+# Deploy Broker
+#cp -f $
+#mv -f
